245f9df194df9a8ea99ec17abd1c3160f08ac282
James <jtronlabs@gmail.com>
2017-08-17 14:07:20 -0400

    working autocomplete, needs to be improved

--- app/assets/javascripts/workout.js
+++ app/assets/javascripts/workout.js
@@ -1,15 +1,16 @@
-let workout_class = '.event_workouts_exercise_name';
+let exercise_name_input = '.event_workouts_exercise_name input';
 
-$(document).ready(init_exercise_autocomplete);
+$(document).ready(function () {
+    init_exercise_autocomplete();
 
-
-$(workout_class).on('cocoon:after-insert', function(e, insertedItem) {
-    breakpoint
+    $('#workout-fields').on('cocoon:after-insert', function (e, insertedItem) {
+        let new_search_input = insertedItem.find(exercise_name_input)
+        init_exercise_autocomplete(new_search_input);
+    });
 });
 
-function init_exercise_autocomplete(){
-    var ex_name_search_el = $(workout_class + ' input'); //attach to input child
-    ex_name_search_el.autocomplete({
-        source: ex_name_search_el.data('autocomplete-source')
+function init_exercise_autocomplete(input_el = $(exercise_name_input)) {
+    input_el.autocomplete({
+        source: input_el.data('autocomplete-source')
     });
 }
