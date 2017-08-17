7ab193e198d69f77a12d05b4e27f9678d3bfda87
James <jtronlabs@gmail.com>
2017-08-17 16:10:24 -0400

    fixed issue in last commit

--- app/assets/javascripts/social_profiles.js
+++ app/assets/javascripts/social_profiles.js
@@ -1,5 +1,5 @@
 //= require rails-coccon-handler
 
 $(document).ready(function () {
-    init_coccon_autocomplete('#social-profile-fields', '.event_social_profile_name input');
+    init_coccon_autocomplete('#social-profile-fields', '.event_social_profiles_name input');
 });
