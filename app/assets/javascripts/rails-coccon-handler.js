170a2ac5e6264435a5475d8c7278ea62c835e230
James <jtronlabs@gmail.com>
2017-08-21 14:27:03 -0400

    better looking, less brittle jquery UI autocomplete

--- app/assets/javascripts/rails-coccon-handler.js
+++ app/assets/javascripts/rails-coccon-handler.js
@@ -1,25 +1,34 @@
 
-function init_coccon_autocomplete(wrapper_selector, input_selector, search_on_focus = false) {
-    //function for adding autocomplete to a given input
-    var add_coccon_autocomplete = function (input_el) {
-        input_el.autocomplete({
-            source: input_el.data('autocomplete-source'),
-            autoFocus: true,
-            minLength: 0,
-        });
-        if (search_on_focus) {
-            input_el.focus(function () {
-                $(this).autocomplete('search', $(this).val())
-            });
+var autocomplete_selecter = ".search_autocomplete";
+
+
+function add_coccon_autocomplete(input_el) {
+    input_el.autocomplete({
+        source: input_el.data('autocomplete-source'),
+        autoFocus: true,
+        minLength: 0,
+        messages: {
+            noResults: '',
+            results: function() {}
         }
+    }).focus(function () {
+        $(this).autocomplete('search', $(this).val())
+    });
+}
+
+$(document).ready(function () {
+    //add autocomplete to form inputs
+    let autocomplete_inputs = $(autocomplete_selecter);
+    for (let dom_node of autocomplete_inputs) {
+        add_coccon_autocomplete($(dom_node));
     }
+});
 
-    //add autocomplete to first coccon input
-    add_coccon_autocomplete($(input_selector));
 
-    //ensure auto-complete is added to all addition coccon-generated inputs
+function init_coccon_autocomplete(wrapper_selector) {
+    //ensure auto-complete is added to all additional coccon-generated inputs
     $(wrapper_selector).on('cocoon:after-insert', function (e, insertedItem) {
-        let new_autocomplete_input = insertedItem.find(input_selector)
+        let new_autocomplete_input = insertedItem.find(autocomplete_selecter)
         add_coccon_autocomplete(new_autocomplete_input);
     });
 }
--- app/assets/javascripts/workout.js
+++ app/assets/javascripts/workout.js
@@ -1,5 +1,5 @@
 //= require rails-coccon-handler
 
 $(document).ready(function () {
-    init_coccon_autocomplete('#workout-fields', '.event_workouts_exercise_name input');
+    init_coccon_autocomplete('#workout-fields');
 });
