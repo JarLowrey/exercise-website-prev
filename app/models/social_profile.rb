1f39213e59007c698bcc6bf61c16a51b4a56048a
James <jtronlabs@gmail.com>
2017-08-29 19:33:13 -0400

    url validation for social profiles

--- app/models/social_profile.rb
+++ app/models/social_profile.rb
@@ -1,3 +1,5 @@
+require 'uri'
+
 class SocialProfile < ApplicationRecord
   belongs_to :shareable, polymorphic: true, optional: true
   
@@ -5,7 +7,7 @@ class SocialProfile < ApplicationRecord
   #validates :url, presence: true, allow_blank: false
 
 
-  validate :all_present
+  validate :all_present, :valid_url
 
   def name=(str)
     super(str.titleize)
@@ -17,6 +19,13 @@ class SocialProfile < ApplicationRecord
     def all_present
       if name.blank? or url.blank?
         errors.add(:base, "Name and URL must both be specified")
+      end
+    end
+    
+    def valid_url
+      uri = URI.parse(url)
+      if !uri.is_a?(URI::HTTP) || uri.host.nil?
+        errors.add(:base, "URL is invalid")
       end
     end
 end
