456d4af9963afd1b36cafc1eddcaeba67d4fd080
James <jtronlabs@gmail.com>
2017-10-06 14:26:12 -0400

    customized start/end forms for calendar searching

--- app/javascript/src/events_search/search_form.jsx
+++ app/javascript/src/events_search/search_form.jsx
@@ -1,22 +1,54 @@
-import React from 'react'
+import React from 'react';
 
-import Datetime from 'react-datetime'
+import Datetime from 'react-datetime';
+import moment from 'moment';
 // import styled from 'styled-components';
 // import '../css/react-datetime.css';
 
 export default class EventSearchForm extends React.Component {
 
+  componentWillMount(){
+    //set default state
+    this.setState({
+      start: Datetime.moment(),
+      end: Datetime.moment().add(1,'week')
+    });
+  }
+
+  _isTodayOrLater(currentDate) {
+    let yesterday = Datetime.moment().subtract(1, 'day');
+    return currentDate.isAfter(yesterday);
+  }
+  _isValidEnd(currentDate) {
+    try {
+      return this._isTodayOrLater(currentDate) && currentDate.isSameOrAfter(this.state.start);
+    } catch (e) {
+      return false;
+    }
+  }
+  
   render() {
-    return <Datetime
-      isValidDate={(currentDate) => {
-        var yesterday = Datetime.moment().subtract(1, 'day');
-        return currentDate.isAfter(yesterday);
-      }}
-      inputProps={
-        {
-          placeHolder: "From",
-          name: "start"
-        }
-      } />;
+    return (
+      <div>
+        <Datetime
+          isValidDate={this._isTodayOrLater}
+          defaultValue={this.state.start}
+          inputProps={
+            {
+              placeholder: "From"
+            }
+          }
+          onChange={(date) => { this.setState({ start: date }) }} />
+        <Datetime
+          isValidDate={this._isValidEnd.bind(this)}
+          defaultValue={this.state.end}
+          inputProps={
+            {
+              placeholder: "To"
+            }
+          }
+          onChange={(date) => { this.setState({ end: date }) }} />
+      </div>
+    );
   }
 }
